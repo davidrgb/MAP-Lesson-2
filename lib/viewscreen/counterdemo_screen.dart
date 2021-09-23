bc62b9e03a492234dc2c397abdef355abc8b9e81
davidrgb <davidrussellrgb@gmail.com>
2021-09-22 19:41:56 -0500

    1.5

---
+++ lib/viewscreen/counterdemo_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class CounterDemoScreen extends StatefulWidget {
+
+  static const routeName = '/counterDemoScreen';
+
+  @override
+  State<StatefulWidget> createState() {
+    return _CounterDemoState();
+  }
+}
+
+class _CounterDemoState extends State<CounterDemoScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Counter Demo'),
+      ),
+      body: Text('counter demo'),
+    );
+  }
+}
---
+++ lib/viewscreen/start_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:lesson2/viewscreen/counterdemo_screen.dart';
+
+class StartScreen extends StatefulWidget {
+  static const routeName = '/startScreen';
+
+  @override
+  State<StatefulWidget> createState() {
+    return _StartScreenState();
+  }
+}
+
+class _StartScreenState extends State<StartScreen> {
+
+  late _Controller con;
+
+  @override
+  void initState() {
+    super.initState();
+    con = _Controller(this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Start Menu'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: con.navigate2CounterDemo,
+              child: Text('Counter Demo'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+class _Controller {
+  late _StartScreenState state;
+  _Controller(this.state);
+
+  void navigate2CounterDemo() {
+    Navigator.pushNamed(state.context, CounterDemoScreen.routeName);
+  }
+}
